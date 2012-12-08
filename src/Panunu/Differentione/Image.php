24f1b8a7fd5c3aa58c10d5914e6f064b4a459fc7
Panu Leppäniemi <me@panuleppaniemi.com>
2012-12-08 18:44:34 +0200

    First test + autoloading via the amazing Composter

---
+++ src/Panunu/Differentione/Image.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Panunu\Differentione;
+
+class Image
+{
+    
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+    if (!@require __DIR__ . '/../vendor/autoload.php') {
+        die('Download Composer and run `php composer.phar install`');
+    }
